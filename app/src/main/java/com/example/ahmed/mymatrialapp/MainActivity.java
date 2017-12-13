da1453ab539c08943b2be874903e00ee0e606069
AhmedElelemy <ahmedkajoahmedkajo@gmail.com>
2017-12-13 20:04:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmed/mymatrialapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ahmed.mymatrialapp;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.xyzreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
